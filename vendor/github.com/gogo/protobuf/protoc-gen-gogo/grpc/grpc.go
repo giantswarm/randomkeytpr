d30333b6c23134aade3841156fa441a2045a957f
Tim Schindler <xh3b4sd@gmail.com>
2018-06-11 12:37:37 +0200

    k8s 1.10.4 (#10)

--- vendor/github.com/gogo/protobuf/protoc-gen-gogo/grpc/grpc.go
+++ vendor/github.com/gogo/protobuf/protoc-gen-gogo/grpc/grpc.go
@@ -36,7 +36,6 @@ package grpc
 
 import (
 	"fmt"
-	"path"
 	"strconv"
 	"strings"
 
@@ -129,16 +128,16 @@ func (g *grpc) GenerateImports(file *generator.FileDescriptor) {
 	if len(file.FileDescriptorProto.Service) == 0 {
 		return
 	}
-	g.P("import (")
-	g.P(contextPkg, " ", strconv.Quote(path.Join(g.gen.ImportPrefix, contextPkgPath)))
-	g.P(grpcPkg, " ", strconv.Quote(path.Join(g.gen.ImportPrefix, grpcPkgPath)))
-	g.P(")")
-	g.P()
+	imports := generator.NewPluginImports(g.gen)
+	for _, i := range []string{contextPkgPath, grpcPkgPath} {
+		imports.NewImport(i).Use()
+	}
+	imports.GenerateImports(file)
 }
 
 // reservedClientName records whether a client name is reserved on the client side.
 var reservedClientName = map[string]bool{
-// TODO: do we need any in gRPC?
+	// TODO: do we need any in gRPC?
 }
 
 func unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
